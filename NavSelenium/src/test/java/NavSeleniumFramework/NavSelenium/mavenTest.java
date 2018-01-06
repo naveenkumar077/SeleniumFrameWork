72149d07a6be56bf15972cd55ffb30a29f891aa7
naveenkumar\Naveen <naveenkumar077@gmail.com>
2018-01-06 23:13:00 +0530

    Selenium

---
+++ NavSelenium/src/test/java/NavSeleniumFramework/NavSelenium/mavenTest.java
@@ -0,0 +1,23 @@
+package NavSeleniumFramework.NavSelenium;
+
+import org.testng.annotations.Test;
+
+public class mavenTest {
+
+	@Test
+	public void mavenTest1() {
+		System.out.println("Maven Test 1");
+	}
+	@Test
+	public void mavenTest2() {
+		System.out.println("Maven Test 2");
+	}
+	@Test
+	public void mavenTest3() {
+		System.out.println("Maven Test 3");
+	}
+	@Test
+	public void mavenTest4() {
+		System.out.println("Maven Test 4");
+	}
+}
---
+++ NavSelenium/src/test/java/SeleniumPractise/maven1Test.java
@@ -0,0 +1,23 @@
+package SeleniumPractise;
+
+import org.testng.annotations.Test;
+
+public class maven1Test {
+
+	@Test
+	public void mavenTest1() {
+		System.out.println("Maven1 Test 1");
+	}
+	@Test
+	public void mavenTest2() {
+		System.out.println("Maven1 Test 2");
+	}
+	@Test
+	public void mavenTest3() {
+		System.out.println("Maven1 Test 3");
+	}
+	@Test
+	public void mavenTest4() {
+		System.out.println("Maven1 Test 4");
+	}
+}
